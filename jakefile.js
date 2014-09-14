3385b3bfc3b10b2e92bcc722897d809ede1fab29
ctrucza <csaba.trucza@gmail.com>
2014-09-14 11:14:50 +0300

    jake runs unit tests asynchronously and fails the build when tests fail

--- jakefile.js
+++ jakefile.js
@@ -20,8 +20,12 @@
     desc("Test everything");
     task("test", [], function(){
         var reporter = require("nodeunit").reporters["default"];
-        reporter.run(["src/server/_server_test.js"]);
-    });
+        reporter.run(["src/server/_server_test.js"], null, function(failures){
+            if (failures)
+                fail("failed tests");
+            complete();
+        });
+    }, {async:true});
 
     desc("Integrate");
     task("integrate", ["default"], function(){
