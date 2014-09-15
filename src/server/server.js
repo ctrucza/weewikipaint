aaebca9197c65f11ffa365576e59e2c7d2e0efe9
ctrucza <csaba.trucza@gmail.com>
2014-09-15 08:44:59 +0300

    test-driving http server

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,13 @@
 "use strict";
 
-exports.number = function()
-{
-    return 3;
+var http = require("http");
+
+exports.start = function(){
+    var server = http.createServer();
+
+    server.on("request", function(request, response){
+        response.end("Hello World!");
+    });
+
+    server.listen(8080);
 };
