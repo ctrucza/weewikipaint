134d19ae6e97d0342ef5e18d2bd0e64dfb3ce2ad
ctrucza <csaba.trucza@gmail.com>
2014-09-18 08:46:24 +0300

    First client-server test working

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -3,11 +3,16 @@
 var server = require("./server.js");
 var http = require("http");
 
-exports.testHttpServer = function(test){
+exports.tearDown = function(callback){
+    server.stop();
+    callback();
+};
+
+exports.testServerRespondsToGetRequests = function(test){
     server.start();
 
     http.get("http://localhost:8080", function(response){
-
+        response.on("data", function(){});
+        test.done();
     });
-    test.done();
 };
--- src/server/server.js
+++ src/server/server.js
@@ -1,13 +1,18 @@
 "use strict";
 
 var http = require("http");
+var server;
 
 exports.start = function(){
-    var server = http.createServer();
+    server = http.createServer();
 
     server.on("request", function(request, response){
-        response.end("Hello World!");
+        response.end();
     });
 
     server.listen(8080);
+};
+
+exports.stop = function(){
+    server.close();
 };
