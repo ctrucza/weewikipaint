ad6f244ac46390395a7c4381185b0dfb3b20abe3
ctrucza <csaba.trucza@gmail.com>
2014-09-21 13:11:44 +0300

    Tests when server is stopped while not running. Requiring port number on server start

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -3,17 +3,8 @@
 var server = require("./server.js");
 var http = require("http");
 
-exports.setUp = function(callback){
-    server.start(8080);
-    callback();
-};
-
-exports.tearDown = function(callback){
-    server.stop();
-    callback();
-};
-
 exports.test_ServerReturnsHelloWorld = function(test){
+    server.start(8080);
     var request = http.get("http://localhost:8080");
     request.on("response", function(response){
         response.setEncoding("utf8");
@@ -26,14 +17,30 @@ exports.test_ServerReturnsHelloWorld = function(test){
         });
         response.on("end", function(){
             test.ok(responseReceived, "should have received data");
-            test.done();
+            server.stop(function(){
+                test.done();
+            });
         });
     });
 };
 
+exports.test_ServerRequiresPortNumber = function(test){
+    test.throws(function(){
+        server.start();
+    });
+    test.done();
+};
+
 exports.test_ServerRunsCallbackWhenStopped = function(test){
+    server.start(8080);
     server.stop(function(){
         test.done();
     });
-    server.start(8080); // TODO: awkward
+};
+
+exports.test_CallingStopWhileServerNotRunningThrows = function(test){
+    test.throws(function(){
+        server.stop();
+    });
+    test.done();
 };
