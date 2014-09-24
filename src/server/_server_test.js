b11f52a400978634dee4a571952640219b1367fb
ctrucza <csaba.trucza@gmail.com>
2014-09-24 08:51:21 +0300

    Serving a static file

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -4,8 +4,14 @@ var server = require("./server.js");
 var http = require("http");
 var fs = require("fs");
 
-exports.test_ServerReturnsHelloWorld = function(test){
-    server.start(8080);
+var TEST_FILE = "generated/test/test.html";
+
+exports.test_ServerServesAFile = function(test){
+    var testData = "This is served from a static file";
+
+    fs.writeFileSync(TEST_FILE, testData);
+
+    server.start(TEST_FILE, 8080);
     var request = http.get("http://localhost:8080");
     request.on("response", function(response){
         response.setEncoding("utf8");
@@ -13,41 +19,37 @@ exports.test_ServerReturnsHelloWorld = function(test){
 
         test.equals(200, response.statusCode);
         response.on("data", function(chunk){
-            test.equals("Hello World", chunk, "expected Hello World!");
+            test.equals(testData, chunk, "expected Hello World!");
             responseReceived = true;
         });
         response.on("end", function(){
             test.ok(responseReceived, "should have received data");
             server.stop(function(){
+                // TODO: this should be run even if the code above fails.
+                fs.unlinkSync(TEST_FILE);
+                test.ok(!fs.existsSync(TEST_FILE), "file should have been deleted");
                 test.done();
             });
         });
     });
 };
 
-exports.test_ServerServesAFile = function(test){
-    var testDir = "generated/test";
-    var testFile = testDir + "/test.html";
-
-    try {
-        fs.writeFileSync(testFile, "Hello World");
-        test.done();
-    }
-    finally {
-        fs.unlinkSync(testFile);
-        test.ok(!fs.existsSync(testFile), "file should have been deleted");
-    }
-};
-
-exports.test_ServerRequiresPortNumber = function(test){
+exports.test_serverNeedsFileToServe = function(test){
     test.throws(function(){
         server.start();
     });
     test.done();
 };
 
+exports.test_ServerRequiresPortNumber = function(test){
+    test.throws(function(){
+        server.start(TEST_FILE);
+    });
+    test.done();
+};
+
 exports.test_ServerRunsCallbackWhenStopped = function(test){
-    server.start(8080);
+    server.start(TEST_FILE, 8080);
     server.stop(function(){
         test.done();
     });
--- src/server/server.js
+++ src/server/server.js
@@ -1,15 +1,20 @@
 "use strict";
 
 var http = require("http");
+var fs = require("fs");
 var server;
 
-exports.start = function(portNumber){
+exports.start = function(htmlFileToServe, portNumber){
+    if (!htmlFileToServe) throw new Error("file to serve is required");
     if (!portNumber) throw new Error("port number is required");
 
     server = http.createServer();
 
     server.on("request", function(request, response){
-        response.end("Hello World");
+        fs.readFile(htmlFileToServe, function(err, data){
+            if (err) throw err;
+            response.end(data);
+        });
     });
 
     server.listen(portNumber);
