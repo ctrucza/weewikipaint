8df4f8f5f7004cb576bada79716c23eea51af973
ctrucza <csaba.trucza@gmail.com>
2014-09-25 09:01:32 +0300

    serving 404 and index.html

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -1,63 +1,98 @@
-"use strict";
+(function(){
 
-var server = require("./server.js");
-var http = require("http");
-var fs = require("fs");
+    "use strict";
 
-var TEST_FILE = "generated/test/test.html";
+    var server = require("./server.js");
+    var http = require("http");
+    var fs = require("fs");
+    var assert = require("assert");
 
-exports.test_serverServesAFile = function(test){
-    var testData = "This is served from a static file";
+    var TEST_FILE = "generated/test/test.html";
 
-    fs.writeFileSync(TEST_FILE, testData);
+    exports.tearDown = function(done)
+    {
+        if (fs.existsSync(TEST_FILE)){
+            fs.unlinkSync(TEST_FILE);
+            assert.ok(!fs.existsSync(TEST_FILE));
+        }
+        done();
+    };
 
-    server.start(TEST_FILE, 8080);
-    var request = http.get("http://localhost:8080");
-    request.on("response", function(response){
-        response.setEncoding("utf8");
-        var responseReceived = false;
+    exports.test_serverServesHomePageFromFile = function(test){
+        var testData = "This is served from a static file";
 
-        test.equals(200, response.statusCode);
-        response.on("data", function(chunk){
-            test.equals(testData, chunk, "expected Hello World!");
-            responseReceived = true;
+        fs.writeFileSync(TEST_FILE, testData);
+
+        httpGet("http://localhost:8080", function(response, responseData){
+            test.equals(200, response.statusCode);
+            test.equals(testData, responseData, "expected Hello World!");
+            test.done();
         });
-        response.on("end", function(){
-            test.ok(responseReceived, "should have received data");
-            server.stop(function(){
-                // TODO: this should be run even if the code above fails.
-                fs.unlinkSync(TEST_FILE);
-                test.ok(!fs.existsSync(TEST_FILE), "file should have been deleted");
-                test.done();
+    };
+
+    exports.test_serverReturnsHomePageWhenAskedForIndex = function(test){
+        var testData = "This is served from a static file";
+
+        fs.writeFileSync(TEST_FILE, testData);
+
+        httpGet("http://localhost:8080/index.html", function(response, responseData){
+            test.equals(200, response.statusCode);
+            test.equals(testData, responseData, "expected Hello World!");
+            test.done();
+        });
+    };
+
+    exports.test_serverReturns404ForEverythingExceptHomePage = function(test){
+        httpGet("http://localhost:8080/noSuchPage", function(response, responseData){
+            test.equals(404, response.statusCode);
+            test.done();
+        });
+    };
+
+    function httpGet(url, callback)
+    {
+        server.start(TEST_FILE, 8080);
+        var request = http.get(url);
+        var responseData = "";
+        request.on("response", function(response){
+            response.setEncoding("utf8");
+
+            response.on("data", function(chunk){
+                responseData += chunk;
+            });
+            response.on("end", function(){
+                server.stop(function(){
+                    callback(response, responseData);
+                });
             });
         });
-    });
-};
+    }
 
-exports.test_serverNeedsFileToServe = function(test){
-    test.throws(function(){
-        server.start();
-    });
-    test.done();
-};
-
-exports.test_serverRequiresPortNumber = function(test){
-    test.throws(function(){
-        server.start(TEST_FILE);
-    });
-    test.done();
-};
-
-exports.test_serverRunsCallbackWhenStopped = function(test){
-    server.start(TEST_FILE, 8080);
-    server.stop(function(){
+    exports.test_serverNeedsFileToServe = function(test){
+        test.throws(function(){
+            server.start();
+        });
         test.done();
-    });
-};
+    };
 
-exports.test_callingStopWhileServerNotRunningThrows = function(test){
-    test.throws(function(){
-        server.stop();
-    });
-    test.done();
-};
+    exports.test_serverRequiresPortNumber = function(test){
+        test.throws(function(){
+            server.start(TEST_FILE);
+        });
+        test.done();
+    };
+
+    exports.test_serverRunsCallbackWhenStopped = function(test){
+        server.start(TEST_FILE, 8080);
+        server.stop(function(){
+            test.done();
+        });
+    };
+
+    exports.test_callingStopWhileServerNotRunningThrows = function(test){
+        test.throws(function(){
+            server.stop();
+        });
+        test.done();
+    };
+}());
