380164d75ffd518f7ec15a04018cc2ca56510cce
ctrucza <csaba.trucza@gmail.com>
2014-09-26 08:33:44 +0300

    Serving custom 404 page

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -7,24 +7,27 @@
     var fs = require("fs");
     var assert = require("assert");
 
-    var TEST_FILE = "generated/test/test.html";
-    var TEST_DATA = "This is served from a static file";
+    var TEST_HOME_PAGE = "generated/test/test.html";
+    var TEST_404_PAGE = "generated/test/test_404.html";
+    var TEST_HOME_PAGE_DATA = "This is the home page";
+    var TEST_404_PAGE_DATA = "This is the 404 page";
 
     exports.setUp = function(done){
-        fs.writeFileSync(TEST_FILE, TEST_DATA);
+        fs.writeFileSync(TEST_HOME_PAGE, TEST_HOME_PAGE_DATA);
+        fs.writeFileSync(TEST_404_PAGE, TEST_404_PAGE_DATA);
         done();
     };
 
     exports.tearDown = function(done){
-        fs.unlinkSync(TEST_FILE);
-        assert.ok(!fs.existsSync(TEST_FILE));
+        cleanUpFile(TEST_HOME_PAGE);
+        cleanUpFile(TEST_404_PAGE);
         done();
     };
 
     exports.serves_home_page_from_file = function(test){
         httpGet("http://localhost:8080", function(response, responseData){
             test.equals(200, response.statusCode);
-            test.equals(TEST_DATA, responseData, "expected Hello World!");
+            test.equals(TEST_HOME_PAGE_DATA, responseData, "expected Hello World!");
             test.done();
         });
     };
@@ -32,7 +35,7 @@
     exports.returns_home_page_when_asked_for_index = function(test){
         httpGet("http://localhost:8080/index.html", function(response, responseData){
             test.equals(200, response.statusCode);
-            test.equals(TEST_DATA, responseData, "expected Hello World!");
+            test.equals(TEST_HOME_PAGE_DATA, responseData, "expected home page content");
             test.done();
         });
     };
@@ -40,26 +43,34 @@
     exports.returns_404_for_everything_except_home_page = function(test){
         httpGet("http://localhost:8080/noSuchPage", function(response, responseData){
             test.equals(404, response.statusCode);
+            test.equals(TEST_404_PAGE_DATA, responseData, "expected 404 page content");
             test.done();
         });
     };
 
-    exports.requires_file_to_serve = function(test){
+    exports.requires_home_page_parameter = function(test){
         test.throws(function(){
             server.start();
         });
         test.done();
     };
 
-    exports.requires_port_number = function(test){
+    exports.requires_404_page_parameter = function(test){
         test.throws(function(){
-            server.start(TEST_FILE);
+            server.start(TEST_HOME_PAGE);
+        });
+        test.done();
+    };
+
+    exports.requires_port_parameter = function(test){
+        test.throws(function(){
+            server.start(TEST_HOME_PAGE, TEST_404_PAGE);
         });
         test.done();
     };
 
     exports.stop_runs_callback = function(test){
-        server.start(TEST_FILE, 8080);
+        server.start(TEST_HOME_PAGE, TEST_404_PAGE, 8080);
         server.stop(function(){
             test.done();
         });
@@ -74,7 +85,7 @@
 
     function httpGet(url, callback)
     {
-        server.start(TEST_FILE, 8080);
+        server.start(TEST_HOME_PAGE, TEST_404_PAGE, 8080);
         var request = http.get(url);
         var responseData = "";
         request.on("response", function(response){
@@ -90,4 +101,10 @@
             });
         });
     }
+
+    function cleanUpFile(fileName){
+        fs.unlinkSync(fileName);
+        assert.ok(!fs.existsSync(fileName));
+    }
+
 }());
--- src/server/server.js
+++ src/server/server.js
@@ -1,33 +1,41 @@
-"use strict";
+(function(){
+    "use strict";
 
-var http = require("http");
-var fs = require("fs");
-var server;
+    var http = require("http");
+    var fs = require("fs");
+    var server;
 
-exports.start = function(htmlFileToServe, portNumber){
-    if (!htmlFileToServe) throw new Error("file to serve is required");
-    if (!portNumber) throw new Error("port number is required");
+    exports.start = function(homePage, notFoundPage, portNumber){
+        if (!homePage) throw new Error("Home page is required");
+        if (!notFoundPage) throw new Error("404 page is required");
+        if (!portNumber) throw new Error("Port number is required");
 
-    server = http.createServer();
+        server = http.createServer();
 
-    server.on("request", function(request, response){
-        if (request.url === "/" || request.url === "/index.html")
-        {
-            fs.readFile(htmlFileToServe, function(err, data){
-                if (err) throw err;
-                response.end(data);
-            });
-        }
-        else
-        {
-            response.statusCode = 404;
-            response.end();
-        }
-    });
+        server.on("request", function(request, response){
+            if (request.url === "/" || request.url === "/index.html")
+            {
+                response.statusCode = 200;
+                serveFile(response, homePage);
+            }
+            else
+            {
+                response.statusCode = 404;
+                serveFile(response, notFoundPage);
+            }
+        });
 
-    server.listen(portNumber);
-};
+        server.listen(portNumber);
+    };
 
-exports.stop = function(callback){
-    server.close(callback);
-};
+    function serveFile(response, file){
+        fs.readFile(file, function(err, data){
+            if (err) throw err;
+            response.end(data);
+        });
+    }
+
+    exports.stop = function(callback){
+        server.close(callback);
+    };
+}());
