86e4eeeef48aff67f44466a3bc5d9dd084b65d9d
ctrucza <csaba.trucza@gmail.com>
2014-10-03 08:01:54 +0300

    smoke test for launching server fails successfully

--- src/_smoke_test.js
+++ src/_smoke_test.js
@@ -2,9 +2,24 @@
 
     "use strict";
 
+    var child_process = require("child_process");
+
     exports.smoke = function(test){
-        test.ok(false, "Woops");
-        test.done();
+        var command = "node weewikipaint 8080";
+        child_process.exec(command, function(error, stdout, stderr){
+            if (error !== null) {
+                console.log(error);
+                console.log(stdout);
+                console.log(stderr);
+                throw error;
+            }
+            console.log("exec");
+            test.done();
+        });
     };
+
+    function runProcess(command){
+
+    }
 
 }());
