a2880f3fe80d3c6277fda7a331719b929d89a9c9
ctrucza <csaba.trucza@gmail.com>
2014-10-12 11:08:48 +0300

    port read from passed parameters inserver startup

--- src/server/weewikipaint.js
+++ src/server/weewikipaint.js
@@ -2,7 +2,8 @@
    "use strict";
 
     var server = require("./server.js");
-    server.start("homepage.html", "404.html", 8080, function(){
+    var port = process.argv[2];
+    server.start("homepage.html", "404.html", port, function(){
         console.log("Server started");
     });
 }());
