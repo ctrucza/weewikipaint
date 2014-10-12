6470d404198c9c5e17cd794916f267a8946d419b
ctrucza <csaba.trucza@gmail.com>
2014-10-12 11:17:25 +0300

    smoke-testing 404 page

--- src/_smoke_test.js
+++ src/_smoke_test.js
@@ -18,15 +18,23 @@
     };
 
     exports.can_get_homepage = function(test){
-        httpGet("http://localhost:8081", function(response, responseData){
-            var homepageMarkerFound = (responseData.indexOf("WeeWikiPaint homepage") !== -1);
-            test.ok(homepageMarkerFound, "homepage should contain WeeWikiPaint marker");
+        httpGet("http://localhost:8080", function(response, responseData){
+            var foundHomepage = (responseData.indexOf("WeeWikiPaint homepage") !== -1);
+            test.ok(foundHomepage, "homepage should contain WeeWikiPaint marker");
+            test.done();
+        });
+    };
+
+    exports.can_get_404_page = function(test){
+        httpGet("http://localhost:8080/no_such_page.html", function(response, responseData){
+            var found404Page = (responseData.indexOf("WeeWikiPaint 404 page") !== -1);
+            test.ok(found404Page, "404 page should contain WeeWikiPaint marker");
             test.done();
         });
     };
 
     function runServer(callback){
-        child = child_process.spawn("node", ["src/server/weewikipaint", "8081"]);
+        child = child_process.spawn("node", ["src/server/weewikipaint", "8080"]);
         child.stdout.setEncoding("utf8");
         child.stdout.on("data", function(chunk){
             if (chunk.trim() === "Server started"){
